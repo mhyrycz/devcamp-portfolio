ef12f45f86b76345499e42c85b963ae0700d781c
mhyrycz <maciek.hyrycz@gmail.com>
2018-07-11 16:20:16 +0200

    sentry added to config and gem file

---
+++ config/initializers/sentry_api.rb
@@ -0,0 +1,5 @@
+SentryApi.configure do |config|
+  config.endpoint = 'https://app.getsentry.com/api/0'
+  config.auth_token = '332f3a83ff5d4a08a70d58234e1b419fce6dfb22805f4afcbc933af1f20e4c7b'
+  config.default_org_slug = 'solarwinds'
+end
