c4ed89e2499892980f1847fee32ede493e3cba00
mhyrycz <maciek.hyrycz@gmail.com>
2018-07-12 14:25:04 +0200

    toggle button added

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Rails.application.routes.draw do
   #get 'pages/home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-
 #'get 'about',' here can be anything written e.g. about -me
-  resources :blogs
+
+
+  resources :blogs do #custom route
+    member do
+      get :toggle_status  #rest method
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home' # instead yay you are using rails (localhost:3000)
