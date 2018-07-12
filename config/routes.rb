2418ca48300363605320dd79ed1bae764e8cd43e
mhyrycz <maciek.hyrycz@gmail.com>
2018-07-12 11:03:00 +0200

    custom route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  #get 'pages/home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+#'get 'about',' here can be anything written e.g. about -me
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home' # instead yay you are using rails (localhost:3000)
+
 end
