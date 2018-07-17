0da59b98bdf13faf3e16bce275f71546ce5060c4
mhyrycz <maciek.hyrycz@gmail.com>
2018-07-17 14:00:29 +0200

    Completed authentication with virtual attributes (attrs) for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first # self specific user e.g. User.last => self is about present instance
+    # split - it will split in array where is space
+    # first - first element in array
+  end
+
+  def last_name
+    self.name.split.last
+  end
+
 end
