8320449113a631abbe8e38e872ecda1dc6e488d2
mhyrycz <maciek.hyrycz@gmail.com>
2018-07-17 10:14:16 +0200

    implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' #override resource default route
   get 'portfolio-angular', to: 'portfolios#angular'
