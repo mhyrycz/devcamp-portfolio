60931d04acb42b688a347f4526f9071bb84c8fa4
mhyrycz <maciek.hyrycz@gmail.com>
2018-07-17 10:19:23 +0200

    custom routes for device

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' } #custom names
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' #override resource default route
   get 'portfolio-angular', to: 'portfolios#angular'
