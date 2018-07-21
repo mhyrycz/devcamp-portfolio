40e70ae7d92403ffc939f5603347367e466dbf3a
mhyrycz <maciek.hyrycz@gmail.com>
2018-07-21 10:25:50 +0200

    implemented some changes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -32,7 +32,7 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
-    
+
   end
 
   def update # updated record jumped to the last
